7a25c13e7525a9b84f97401c81839df01b441670
tunsay <tuna1997@hotmail.fr>
2022-09-01 15:44:08 +0200

    Mange the error 404 for fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,8 +1,42 @@
 async function getPhotographers() {
 
-    const fetchPromise = await fetch("../data/photographers.json");
-    const photographersGet = await fetchPromise.json();
+
+    const urlFetch = fetch("../../data/photographers.json")
+        .then(response => {
+            if (response.ok) {
+                let json = response.json();
+                return json;
+            } else if (response.status === 404) {
+                return Promise.reject('error 404')
+            } else {
+                return Promise.reject('some other error: ' + response.status)
+            }
+        })
+        .then(photographers => { return photographers })
+        .catch(error => console.log('error is', error));
+
+
+    // function load(url) {
+    //     return new Promise(async function (resolve, reject) {
+    //         // do async thing
+    //         const res = await fetch(url)
+
+    //         // your custom code
+    //         console.log('Yay! Loaded:', url)
+
+    //         // resolve
+    //         resolve(res.json()) // see note below!
+    //     })
+    // }
+
+    // const promise = load('../../data/photographers.json')
+
+    // console.log(await promise);
+
+    const photographersGet = await urlFetch;
+    // const photographersGet = await fetchPromise.json();
     const photographers = photographersGet['photographers'];
+
 
     return ({
         photographers: [...photographers]
