0c0b845c3af2068c30c628ec8103e526f743919b
tunsay <tuna1997@hotmail.fr>
2022-09-03 16:42:12 +0200

    Intergrate the main page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,44 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
+    const link = 'photographer.html';
 
     function getUserCardDOM() {
+        // create <article></article>
         const article = document.createElement( 'article' );
+        
+        //create <a></a>
+        const a = document.createElement('a');
+        a.setAttribute("href", link);
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        img.setAttribute("src", picture);
+
+        const namesGet = document.createElement( 'h2' );
+
+        const cityCountryGet = document.createElement('div');
+        cityCountryGet.classList.add('city-and-country');
+
+        const taglineGet = document.createElement('div');
+        taglineGet.classList.add('tagline');
+        
+        const priceGet = document.createElement('div');
+        priceGet.classList.add('price');
+
+        //display information in HTML
+        namesGet.textContent = name;
+        cityCountryGet.textContent = city + ", " + country;
+        taglineGet.textContent = tagline;
+        priceGet.textContent = price + "€/jour";
+
+        //set Element with appendChild
+        article.appendChild(a);
+        a.appendChild(img);
+        article.appendChild(namesGet);
+        article.appendChild(cityCountryGet);
+        article.appendChild(taglineGet);
+        article.appendChild(priceGet);
         return (article);
     }
     return { name, picture, getUserCardDOM }
